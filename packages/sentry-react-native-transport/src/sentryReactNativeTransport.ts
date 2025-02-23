6ad4fd1ac09c5841844f201d8fd41ec729f95a8f
Koen Gommers <koen@koengommers.nl>
2025-02-23 20:17:46 +0100

    test: add tests for Sentry React Native transport

---
+++ packages/sentry-react-native-transport/src/sentryReactNativeTransport.ts
@@ -0,0 +1,36 @@
+import * as Sentry from "@sentry/react-native";
+import { type Level, createTransport } from "etholog";
+
+type LevelMap = Record<Level, Sentry.SeverityLevel>;
+
+export type SentryReactNativeTransportOptions = {
+  levelMap?: LevelMap;
+};
+
+const DEFAULT_LEVEL_MAP: LevelMap = {
+  error: "error",
+  warn: "warning",
+  info: "info",
+  debug: "debug",
+} as const;
+
+export function sentryReactNativeTransport(
+  options?: SentryReactNativeTransportOptions,
+) {
+  const levelMap = options?.levelMap ?? DEFAULT_LEVEL_MAP;
+
+  return createTransport(
+    (log) => {
+      Sentry.addBreadcrumb({
+        message: log.message,
+        level: levelMap[log.level],
+        data: log.data,
+      });
+    },
+    {
+      flush: async () => {
+        await Sentry.flush();
+      },
+    },
+  );
+}
